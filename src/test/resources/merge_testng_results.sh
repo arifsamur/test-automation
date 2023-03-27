4390b4b04823e12a41c883463d7892ca66ba34cd
Mohammed Gesreha <mohammed.gesreha@caresyntax.com>
2023-03-27 11:55:13 +0000

    Update merge_testng_results.sh

--- src/test/resources/merge_testng_results.sh
+++ src/test/resources/merge_testng_results.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 /path/to/your/folder"
+  exit 1
+fi
+
 input_directory="$1"
-output_file="testng-results.xml"
+
+if [ ! -d "$input_directory" ]; then
+  echo "Directory not found: $input_directory"
+  exit 1
+fi
+
+output_file="${input_directory}/testng-results.xml"
 
 # Initialize the variables for the root data
 ignored=0
@@ -9,23 +20,22 @@ total=0
 passed=0
 failed=0
 skipped=0
-echo 'Create the merged output file with the XML declaration'
-pwd
+
 # Create the merged output file with the XML declaration
 echo '<?xml version="1.0" encoding="UTF-8"?>' > $output_file
 echo '<testng-results>' >> $output_file
-echo 'End the merged output file with the XML declaration'
+
 # Find and process each input file
 while IFS= read -r file; do
-    # Accumulate the root data values
-    ignored=$((ignored + $(xmlstarlet sel -t -v '/testng-results/@ignored' "$file")))
-    total=$((total + $(xmlstarlet sel -t -v '/testng-results/@total' "$file")))
-    passed=$((passed + $(xmlstarlet sel -t -v '/testng-results/@passed' "$file")))
-    failed=$((failed + $(xmlstarlet sel -t -v '/testng-results/@failed' "$file")))
-    skipped=$((skipped + $(xmlstarlet sel -t -v '/testng-results/@skipped' "$file")))
+  # Accumulate the root data values
+  ignored=$((ignored + $(xmlstarlet sel -t -v '/testng-results/@ignored' "$file")))
+  total=$((total + $(xmlstarlet sel -t -v '/testng-results/@total' "$file")))
+  passed=$((passed + $(xmlstarlet sel -t -v '/testng-results/@passed' "$file")))
+  failed=$((failed + $(xmlstarlet sel -t -v '/testng-results/@failed' "$file")))
+  skipped=$((skipped + $(xmlstarlet sel -t -v '/testng-results/@skipped' "$file")))
 
-    # Extract the suite elements and append them to the merged output file
-    xmlstarlet sel -t -c '/testng-results/suite' "$file" >> $output_file
+  # Extract the suite elements and append them to the merged output file
+  xmlstarlet sel -t -c '/testng-results/suite' "$file" >> $output_file
 done < <(find "$input_directory" -name "testng_result_*.xml")
 
 # Add the accumulated root data values to the merged output file
