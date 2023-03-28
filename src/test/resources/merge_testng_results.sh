8ad094b333c132cec8f75a29c511383fe522d321
Mohammed Gesreha <mohammed.gesreha@caresyntax.com>
2023-03-28 12:55:54 +0000

    Update merge_testng_results.sh

--- src/test/resources/merge_testng_results.sh
+++ src/test/resources/merge_testng_results.sh
@@ -1,42 +1,16 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 /path/to/your/folder"
-  exit 1
-fi
+# Create a new XML file with the root element
+echo '<?xml version="1.0" encoding="UTF-8"?><suite>' > merged-results.xml
 
-input_directory="$1"
+# Loop through all testng-results_*.xml files in the current directory
+for file in testng-results_*.xml; do
+  # Check if the file exists and is an XML file
+  if [[ -f "$file" && "$file" == *.xml ]]; then
+    # Extract the <test> elements from each file and append them to the merged-results.xml file
+    xmlstarlet sel -t -m '//test' -c . -n "$file" >> merged-results.xml
+  fi
+done
 
-if [ ! -d "$input_directory" ]; then
-  echo "Directory not found: $input_directory"
-  exit 1
-fi
-
-output_file="${input_directory}/testng-results.xml"
-
-# Initialize the variables for the root data
-ignored=0
-total=0
-passed=0
-failed=0
-skipped=0
-
-# Create the merged output file with the XML declaration
-echo '<?xml version="1.0" encoding="UTF-8"?>' > $output_file
-echo '<testng-results>' >> $output_file
-
-# Find and process each input file
-while IFS= read -r file; do
-  # Accumulate the root data values
-  ignored=$((ignored + $(xmlstarlet sel -t -v '/testng-results/@ignored' "$file")))
-  total=$((total + $(xmlstarlet sel -t -v '/testng-results/@total' "$file")))
-  passed=$((passed + $(xmlstarlet sel -t -v '/testng-results/@passed' "$file")))
-  failed=$((failed + $(xmlstarlet sel -t -v '/testng-results/@failed' "$file")))
-  skipped=$((skipped + $(xmlstarlet sel -t -v '/testng-results/@skipped' "$file")))
-
-  # Extract the suite elements and append them to the merged output file
-  xmlstarlet sel -t -c '/testng-results/suite' "$file" >> $output_file
-done < <(find "$input_directory" -name "testng_result_*.xml")
-
-# Add the accumulated root data values to the merged output file
-xmlstarlet ed -L -u '/testng-results/@ignored' -v $ignored -u '/testng-results/@total' -v $total -u '/testng-results/@passed' -v $passed -u '/testng-results/@failed' -v $failed -u '/testng-results/@skipped' -v $skipped $output_file
+# Close the root element
+echo '</suite>' >> merged-results.xml
