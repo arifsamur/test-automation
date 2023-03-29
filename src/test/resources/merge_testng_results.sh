c1f911a68b97031004fc9bbee7782842526a8676
Mohammed Gesreha <mohammed.gesreha@caresyntax.com>
2023-03-29 10:29:01 +0000

    Update merge_testng_results.sh

--- src/test/resources/merge_testng_results.sh
+++ src/test/resources/merge_testng_results.sh
@@ -1,39 +1,45 @@
 #!/bin/bash
 
-# Create a new XML file with the root element
-# echo '<?xml version="1.0" encoding="UTF-8"?><testng-results><suite>' > merged-results.xml
+# Initialize variables for attribute totals
+total_ignored=0
+total_total=0
+total_passed=0
+total_failed=0
+total_skipped=0
 
-# Loop through all testng-results_*.xml files in the current directory
-for file in testng-results_*.xml; do
- # Extract the attributes from each file and update the attribute variables
-    ignored=$(xmlstarlet sel -t -v '/testng-results/@ignored' "$file")
-    total=$(xmlstarlet sel -t -v '/testng-results/@total' "$file")
-    passed=$(xmlstarlet sel -t -v '/testng-results/@passed' "$file")
-    failed=$(xmlstarlet sel -t -v '/testng-results/@failed' "$file")
-    skipped=$(xmlstarlet sel -t -v '/testng-results/@skipped' "$file")
+# Get the list of testng-results_*.xml files
+files_list=$(find . -name "testng-results_*.xml")
 
-    total_ignored=$((total_ignored + ignored))
-    total_total=$((total_total + total))
-    total_passed=$((total_passed + passed))
-    total_failed=$((total_failed + failed))
-    total_skipped=$((total_skipped + skipped))
-done
-
-first_file=$(find . -name "testng-results_*.xml" | head -n 1)
+# Get the first file from the list
+first_file=$(echo "$files_list" | head -n 1)
 
 # If a first file is found, copy it to merged-results.xml
 if [ -n "$first_file" ]; then
   cp "$first_file" merged-results.xml
+
+  # Extract the attribute values from the first file and add them to the totals
+  total_ignored=$((total_ignored + $(xmlstarlet sel -t -v "//testng-results/@ignored" "$first_file")))
+  total_total=$((total_total + $(xmlstarlet sel -t -v "//testng-results/@total" "$first_file")))
+  total_passed=$((total_passed + $(xmlstarlet sel -t -v "//testng-results/@passed" "$first_file")))
+  total_failed=$((total_failed + $(xmlstarlet sel -t -v "//testng-results/@failed" "$first_file")))
+  total_skipped=$((total_skipped + $(xmlstarlet sel -t -v "//testng-results/@skipped" "$first_file")))
 fi
 
 # Loop through all testng-results_*.xml files except the first one
-for file in $(find . -name "testng-results_*.xml" | tail -n +2); do
+for file in $(echo "$files_list" | tail -n +2); do
   # Check if the file exists and is an XML file
   if [[ -f "$file" && "$file" == *.xml ]]; then
     # Extract the <test> elements from each file
     tests_to_insert=$(xmlstarlet sel -t -m '//test' -c . -n "$file")
     # Insert the <test> elements after the last <test> element in the merged-results.xml file
-    xmlstarlet ed -L -a "//testng-results/test[last()]" -t xml -v "$tests_to_insert" merged-results.xml
+    xmlstarlet ed -L -a "//suite" -t xml -v "$tests_to_insert" merged-results.xml
+
+    # Extract the attribute values from each file and add them to the totals
+    total_ignored=$((total_ignored + $(xmlstarlet sel -t -v "//testng-results/@ignored" "$file")))
+    total_total=$((total_total + $(xmlstarlet sel -t -v "//testng-results/@total" "$file")))
+    total_passed=$((total_passed + $(xmlstarlet sel -t -v "//testng-results/@passed" "$file")))
+    total_failed=$((total_failed + $(xmlstarlet sel -t -v "//testng-results/@failed" "$file")))
+    total_skipped=$((total_skipped + $(xmlstarlet sel -t -v "//testng-results/@skipped" "$file")))
   fi
 done
 
@@ -45,4 +51,3 @@ echo "TOTAL_TOTAL=$total_total" >> results.env
 echo "TOTAL_PASSED=$total_passed" >> results.env
 echo "TOTAL_FAILED=$total_failed" >> results.env
 echo "TOTAL_SKIPPED=$total_skipped" >> results.env
-
